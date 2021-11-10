a490252c8982725e403218a167ba76aac7342b86
developerlin123 <developerlin123@gmail.com>
2021-11-10 12:46:53 +0100

    fake data, structrue

---
+++ src/store.js
@@ -0,0 +1,117 @@
+export const programTypes=[
+    "Series","Movies","Cartoons","Shows","Ads"
+]
+
+export const programmes=[
+
+    {
+        id:"10001",
+        title:"Breaking Bad",
+        synopsis:"A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BMjhiMzgxZTctNDc1Ni00OTIxLTlhMTYtZTA3ZWFkODRkNmE2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",
+        duration:"52min",
+        type:"Series",
+        episode:"1",
+        chapter:"6"
+    },
+    {
+        id:"10002",
+        title:"Lost",
+        synopsis:"The survivors of a plane crash are forced to work together in order to survive on a seemingly deserted tropical island..",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BNzhlY2E5NDUtYjJjYy00ODg3LWFkZWQtYTVmMzU4ZWZmOWJkXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg",
+        duration:"52min",
+        type:"Series",
+        episode:"2",
+        chapter:"10"
+    },
+    {
+        id:"10003",
+        title:"Squid Game",
+        synopsis:"Hundreds of cash-strapped players accept a strange invitation to compete in children's games. Inside, a tempting prize awaits with deadly high stakes. A survival game that has a whopping 45.6 billion-won prize at stake.",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BYWE3MDVkN2EtNjQ5MS00ZDQ4LTliNzYtMjc2YWMzMDEwMTA3XkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_FMjpg_UX1000_.jpg",
+        duration:"49min",
+        type:"Series",
+        episode:"1",
+        chapter:"5"
+    },
+    {
+        id:"20001",
+        title:"Life of Pi",
+        synopsis:"A young man who survives a disaster at sea is hurtled into an epic journey of adventure and discovery. While cast away, he forms an unexpected connection with another survivor: a fearsome Bengal tiger.",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BNTg2OTY2ODg5OF5BMl5BanBnXkFtZTcwODM5MTYxOA@@._V1_FMjpg_UX1000_.jpg",
+        duration:"180min",
+        type:"Movies",
+        episode:"",
+        chapter:""
+    },
+    {
+        id:"20002",
+        title:"The Invisible Guest",
+        synopsis:"A successful entrepreneur accused of murder and a witness preparation expert have less than three hours to come up with an impregnable defense.",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BMDk0YzAwYjktMWFiZi00Y2FmLWJmMmMtMzUyZDZmMmU5MjkzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg",
+        duration:"120min",
+        type:"Movies",
+        episode:"",
+        chapter:""
+    },
+    {
+        id:"30001",
+        title:"The Simpsons",
+        synopsis:"The satiric adventures of a working-class family in the misfit city of Springfield.",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
+        duration:"20min",
+        type:"Cartoons",
+        episode:"",
+        chapter:""
+    },
+    {
+        id:"30002",
+        title:"The Croods",
+        synopsis:"After their cave is destroyed, a caveman family must trek through an unfamiliar fantastical world with the help of an inventive boy.",
+        imageUrl:"https://m.media-amazon.com/images/M/MV5BMTcyOTc2OTA1Ml5BMl5BanBnXkFtZTcwOTI1MjkzOQ@@._V1_FMjpg_UX1000_.jpg",
+        duration:"120min",
+        type:"Cartoons",
+        episode:"",
+        chapter:""
+    },
+    {
+        id:"40001",
+        title:"Supervivientes 2021",
+        synopsis:"Supervivientes 2021 todavía no ha lanzado ese esperado «bombazo» que Lara Álvarez prometía que saldría a la luz a pocos días de su inicio, pero ya ha anunciado la primera gran novedad de su próxima edición.",
+        imageUrl:"https://www.elindependiente.com/wp-content/uploads/2021/04/Logo-Supervivientes_2327177263_15453451_660x372-656x368.jpg",
+        duration:"120min",
+        type:"Shows",
+        episode:"4",
+        chapter:"5"
+    },
+    {
+        id:"40002",
+        title:"Wild Frank",
+        synopsis:"Who is Frank Cuesta? What does he do? How can he be so comfortable dealing with wild animals? Watch everything you want to know about...",
+        imageUrl:"https://pbs.twimg.com/media/DsSbVSkWsAAzS1L.jpg",
+        duration:"40min",
+        type:"Shows",
+        episode:"4",
+        chapter:"2"
+    },
+    {
+        id:"50001",
+        title:"Mercadona: Algo más que palabras",
+        synopsis:"El “Jefe”, SPB, calidad total, innovación, interproveedor, producto recomendado, reputación, ética, carro menú o...",
+        imageUrl:"http://javierpanzano.com/wp-content/uploads/2009/09/mercadona-blog.jpg",
+        duration:"0.5min",
+        type:"Ads",
+        episode:"",
+        chapter:""
+    },
+    {
+        id:"50002",
+        title:"Taco rico",
+        synopsis:"Comida rápida restaurante redes sociales publicidad marco frontera fondo menú lista aperitivo comida plantilla diseño creativo acuarela vector ilustración diseño...",
+        imageUrl:"https://www.crushpixel.com/big-static13/preview4/fast-food-restaurant-social-media-1121401.jpg",
+        duration:"0.5min",
+        type:"Ads",
+        episode:"",
+        chapter:""
+    },
+]
